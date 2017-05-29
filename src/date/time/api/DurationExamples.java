11eb49ef3458761473deeefc866c73bdc178a944
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2017-05-29 21:49:06 +0200

    java 8 Duration & Period classes

---
+++ src/date/time/api/DurationExamples.java
@@ -0,0 +1,18 @@
+package date.time.api;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class DurationExamples {
+
+    public static void main(String[] args) {
+        Duration fiveMinutes = Duration.ofMinutes(5);
+        Duration sevenMinutes = fiveMinutes.plusMinutes(2);
+        System.out.println(sevenMinutes.getSeconds());
+
+        LocalDateTime firstMay = LocalDateTime.of(2017, 5, 1, 0, 0);
+        LocalDateTime firstJune = LocalDateTime.of(2017, 6, 1, 0, 0);
+        Duration oneMonth = Duration.between(firstMay, firstJune);
+        System.out.println(oneMonth.getSeconds());
+    }
+}
---
+++ src/date/time/api/PeriodExamples.java
@@ -0,0 +1,19 @@
+package date.time.api;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class PeriodExamples {
+
+    public static void main(String[] args) {
+        Period fiveMonths = Period.ofMonths(5);
+        Period threeMonths = Period.ofMonths(3);
+        Period eightMonths = fiveMonths.plus(threeMonths);
+        System.out.println(eightMonths.getMonths());
+
+        LocalDate localDate1 = LocalDate.of(1990, 1, 1);
+        LocalDate localDate2 = LocalDate.of(1992, 1, 1);
+        Period twoYears = Period.between(localDate1, localDate2);
+        System.out.println(twoYears.getYears());
+    }
+}
