cfe2686f4ead6abe443b2232b8e548410472d201
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2017-05-29 22:02:05 +0200

    java 8 LocalDate class

---
+++ src/date/time/api/LocalDateExamples.java
@@ -0,0 +1,20 @@
+package date.time.api;
+
+import java.time.LocalDate;
+
+public class LocalDateExamples {
+
+    public static void main(String[] args) {
+        LocalDate localDate1 = LocalDate.of(2017, 5, 29);
+        System.out.println(localDate1);
+
+        LocalDate localDate2 = localDate1.plusYears(2);
+        System.out.println(localDate2);
+
+        LocalDate localDate3 = localDate2.plusMonths(3);
+        System.out.println(localDate3);
+
+        LocalDate localDate4 = localDate3.minusDays(4);
+        System.out.println(localDate4);
+    }
+}
